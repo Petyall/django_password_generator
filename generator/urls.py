1093f5a611525959ad0100743722c3cb14052328
Petyall <alekolen35@gmail.com>
2022-09-25 19:41:26 +0300

    password validation working

--- generator/urls.py
+++ generator/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('password', views.password, name='password')
+    path('password', views.password, name='password'),
+    path('faq', views.faq, name='faq'),
+    path('check', views.check, name='check')
 ]
--- generator/views.py
+++ generator/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 import random
+import string
+
 
 def home(request):
     return render(request, 'index.html')
+
+
+def faq(request):
+    return render(request, 'faq.html')
+
 
 def password(request):
     users_password = ''
@@ -15,11 +22,43 @@ def password(request):
         combination.extend(list('0123456789'))
     if request.GET.get('special'):
         combination.extend(list('!#$%&()*+,-./:;<=>?@[\]^_{|}~'))
-    
+
     for i in range(length):
         users_password += random.choice(combination)
     context = {
         'password': users_password
     }
-    
+
     return render(request, 'generated_password.html', context)
+
+
+def check(request):
+    password = request.POST.get('password')
+    length = len(password)
+
+    if length <= 8:
+        output = f'Увеличьте длину пароля, минимум на {12 - length} символов'
+    else:
+        lower = any(
+            [1 if i in string.ascii_lowercase else 0 for i in password])
+        upper = any(
+            [1 if i in string.ascii_uppercase else 0 for i in password])
+        special = any([1 if i in string.punctuation else 0 for i in password])
+        number = any([1 if i in string.digits else 0 for i in password])
+        list = [lower, upper, special, number]
+
+        if all(list):
+            output = 'Ваш пароль надёжный'
+        else:
+            output = ('Рекомендую добавить один или более символов из ' +
+                      'букв нижнего регистра, '*(list[0] is False) +
+                      'букв верхнего регистра, '*(list[1] is False) +
+                      'специальных (!&? и т.д.), '*(list[2] is False) +
+                      'цифр от 0 до 9, '*(list[3] is False) +
+                      'и затем попробуйте снова!'
+                      )
+    context = {
+        'output': output,
+    }
+
+    return render(request, 'check.html', context)
